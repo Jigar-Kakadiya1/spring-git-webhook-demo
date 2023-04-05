f9b4f3cb9ef75e5d1830bb6922a6f1969e6dc2ce
Jigar Kakadiya <jigar.kakadiya@prama.ai>
2023-04-05 07:13:02 +0530

    initial commit

---
+++ src/main/java/com/example/webhookdemo/UserController.java
@@ -0,0 +1,18 @@
+package com.example.webhookdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/user")
+    public String user() {
+        return "jigar kakadiya";
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "welcome to home page";
+    }
+}
